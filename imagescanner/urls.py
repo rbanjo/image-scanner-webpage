8631b5fbc88589374d2b0bcd46650fa16ccbe05d
Abhishek Shrinivasan <abhishek.shrinivasan@gmail.com>
2017-06-28 15:13:54 -0500

    added some code regarding views.py

--- imagescanner/urls.py
+++ imagescanner/urls.py
@@ -6,5 +6,5 @@ from .views import ocr_view,ocr_form_view
 
 urlpatterns = [
     url(r'^$', ocr_form_view, name='ocr_form_view'),
-    url(r'^ocr/', ocr_view, name='ocr_view'),
+    url(r'^ocr/$', ocr_view, name='ocr_view'),
 ]
