1071d4d985b7c2a73b3d096300ac9be92706dd14
Abhishek Shrinivasan <abhishek.shrinivasan@gmail.com>
2017-06-29 08:33:51 -0500

    added code to convert pdf to image files using imagemagick and wand

--- imagescanner/views.py
+++ imagescanner/views.py
@@ -3,7 +3,12 @@ from django.http import HttpResponse
 from django.http.response import JsonResponse
 from django.views.generic.base import View, TemplateView
 from django.views.decorators.csrf import csrf_exempt
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+import os
+from django.conf import settings
 from PIL import Image, ImageFilter
+from wand.image import Image as IMG
 import pytesseract
 
 # Create your views here.
@@ -11,12 +16,26 @@ class OcrFormView(TemplateView):
     template_name='ocr_form.html'
 ocr_form_view = OcrFormView.as_view()
 
+def get_string(name):
+    img=Image.open(name)
+    sharpened_image = img.filter(ImageFilter.SHARPEN)
+    utf8_text = pytesseract.image_to_string(sharpened_image)
+    return utf8_text
+
 class OcrView(View):
     def post(self, request):
-        img=Image.open(request.FILES['image'])
-        sharpened_image = img.filter(ImageFilter.SHARPEN)
-        utf8_text=pytesseract.image_to_string(sharpened_image)
-        print(utf8_text)
+        if("pdf" not in str(request.FILES['image'])):
+            utf8_text=get_string(request.FILES['image'])
+            print(utf8_text)
+        else:
+            data=request.FILES['image']
+            default_storage.save(data.name, ContentFile(data.read()))
+            img=IMG(filename=data.name,resolution=200)
+            img.save(filename='temp.jpg')
+            utf8_text=get_string('temp.jpg')
+            print(utf8_text)
         return JsonResponse({'utf8_text': utf8_text})
+
+
 
 ocr_view = csrf_exempt(OcrView.as_view())
